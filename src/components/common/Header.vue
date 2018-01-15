2a14a8f7570468d97edb1aad03298ed54e5f076c
emily <emily.yan@blockshine.com>
2018-01-15 10:52:07 +0800

    initpage

---
+++ src/components/common/Header.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="header">
+        <div class="logo">BME区块链</div>
+        <div class="sel-info">
+            <!--<el-input-->
+                <!--placeholder="请输入内容"-->
+                <!--prefix-icon="el-icon-search"-->
+                <!--v-model="seltext">-->
+            <!--</el-input>-->
+            <!--<el-dropdown trigger="click" @command="handleCommand">-->
+                <!--<span class="el-dropdown-link">-->
+                    <!--<img class="user-logo" src="../../../static/img/img.jpg">-->
+                    <!--{{username}}-->
+                <!--</span>-->
+                <!--<el-dropdown-menu slot="dropdown">-->
+                    <!--<el-dropdown-item command="loginout">退出</el-dropdown-item>-->
+                <!--</el-dropdown-menu>-->
+            <!--</el-dropdown>-->
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+            //     seltext:'',
+                name: 'Emily'
+            }
+        },
+        computed:{
+        },
+        methods:{
+        }
+    }
+</script>
+<style scoped>
+    .header {
+        position: relative;
+        box-sizing: border-box;
+        width: 100%;
+        height: 70px;
+        font-size: 22px;
+        line-height: 70px;
+        color: #fff;
+    }
+    .header .logo{
+        float: left;
+        width:250px;
+        text-align: center;
+    }
+    .sel-info {
+        float: right;
+        padding-right: 50px;
+        font-size: 16px;
+        color: #fff;
+        border:0px;
+    }
+</style>
---
+++ src/components/page/accountNum.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <div class="ms-doc">
+            <article class="markdown-section" id="main"><p>为将区块链性能提升至全新高度，NEM真正100%全新代码。使开发者可以迅速和节省成本部署基于区块链的应用。NEM提供了可靠签名合约保障极高的安全性。由多层架构保障高吞吐量的实现，使应用仅在区块链层面存储核心信息，使区块链层高效安全运营。私有链和公有链的无缝集成，用户可以选择私网环境独立部署，也可以选择公网环境与公有链连接达成更佳的数据管理。</p>
+                <h3>易用性</h3>
+                <ul>
+                <li>极短块时间</li>
+                <li>钱包具备连接远程节点快速开始的功能，无需加载完整链</li>
+            </ul>
+                <h3>安全性和用户保护</h3>
+                <ul>
+                    <li>NEM的360块后完全不可变特性使得POW或POS区块链存在的长期入侵成功可能性成为过去</li>
+                    <li>NEM使用 Eigentrust++算法评估节点质量，踢出不健康的低声望节点，保护区块链健壮性</li>
+                </ul>
+                <h3>可达性</h3>
+                <ul>
+                    <li>可以自主选择连接全球范围内任意一个NEM网络中的节点，也可以由带有View功能的DNS连接公共推荐节点，有利于轻量或移动APP的实施</li>
+                    <li>NEM软件在几秒钟内就能安装（或解压）完成开启使用，连接远程节点后可立即开始区块链操作</li>
+                </ul>
+            </article>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+    .ms-doc{
+        width:100%;
+        max-width: 980px;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;
+        color: #48576a;
+    }
+    .ms-doc h3{
+        padding: 9px 10px 10px;
+        line-height: 17px;
+        background-color: #f5f5f5;
+        border: 0px solid #d8d8d8;
+        border-bottom: 0;
+        border-radius: 3px 3px 0 0;
+    }
+    .ms-doc article{
+        padding: 20px;
+        word-wrap: break-word;
+        background-color: #fff;
+        border: 0px solid #ddd;
+        border-bottom-right-radius: 3px;
+        border-bottom-left-radius: 3px;
+    }
+    .ms-doc article h1{
+        font-size:32px;
+        padding-bottom: 10px;
+        margin-bottom: 15px;
+        border-bottom: 1px solid #ddd;
+    }
+    .ms-doc article h2 {
+        margin: 24px 0 16px;
+        font-weight: 600;
+        line-height: 1.25;
+        padding-bottom: 7px;
+        font-size: 24px;
+        border-bottom: 1px solid #eee;
+    }
+    .ms-doc article p{
+        margin-bottom: 15px;
+        line-height: 1.5;
+    }
+    .ms-doc article .el-checkbox{
+        margin-bottom: 5px;
+    }
+</style>
