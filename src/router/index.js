55bf1c2c371908ae5d8baea880f8f23749542b8d
emily <emily.yan@blockshine.com>
2018-01-15 17:09:37 +0800

    initdev

---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            redirect: '/home'
+        },
+        {
+            path: '/home',
+            component: resolve => require(['../components/common/Home.vue'], resolve),
+            children:[
+                {
+                    path: '/',
+                    component: resolve => require(['../components/page/proDescription.vue'], resolve)
+                },
+                {
+                    path: '/basicConcept',
+                    component: resolve => require(['../components/page/basicConcept.vue'], resolve)
+                },
+                {
+                    path: '/reqInter',
+                    component: resolve => require(['../components/page/reqInter.vue'], resolve)
+                },
+                {
+                    path: '/server',
+                    component: resolve => require(['../components/page/server.vue'], resolve)
+                },
+                {
+                    path: '/accountNum',
+                    component: resolve => require(['../components/page/accountNum.vue'], resolve)
+                },
+                {
+                    path: '/blockChain',
+                    component: resolve => require(['../components/page/blockChain.vue'], resolve)
+                },
+                {
+                    path: '/node',
+                    component: resolve => require(['../components/page/node.vue'], resolve)
+                },
+                {
+                    path: '/nameWithmos',
+                    component: resolve => require(['../components/page/nameWithmos.vue'], resolve)
+                },
+                {
+                    path: '/transaction',
+                    component: resolve => require(['../components/page/transaction.vue'], resolve)
+                }
+            ]
+        }
+    ]
+})
