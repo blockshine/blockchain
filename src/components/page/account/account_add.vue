e6b8e2ec047dd8af9fe33181d2933add19ef42cf
yanyungit <464713274@qq.com>
2018-01-16 17:11:34 +0800

    创建账户示例

--- src/components/page/account/account_add.vue
+++ src/components/page/account/account_add.vue
@@ -2,12 +2,50 @@
     <div>
         <div class="ms-doc">
             <article class="markdown-section" id="main">
-            <h3>创建账户</h3>
-            <li>请求参数</li>
-            <li>请求url</li>
-            <li>响应参数</li>
-            <li>请求示例</li>
-            <li>响应示例</li>
+                <h3>创建账户</h3>
+                <p><strong>请求语法</strong></p>
+                <pre v-pre="" data-lang=""><code class="lang-">GET /account/generate</code></pre>
+                <p><strong>请求参数</strong></p>
+                <p>无</p>
+                <p><strong>响应参数</strong></p>
+                <table>
+                    <thead>
+                    <tr>
+                        <th style="text-align:left">参数</th>
+                        <th style="text-align:left">类型</th>
+                        <th style="text-align:left">说明</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td style="text-align:left">privateKey</td>
+                        <td style="text-align:left">String</td>
+                        <td style="text-align:left">返回新创建账户的私匙</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:left">address</td>
+                        <td style="text-align:left">String</td>
+                        <td style="text-align:left">返回新创建账户的地址</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:left">publicKey</td>
+                        <td style="text-align:left">String</td>
+                        <td style="text-align:left">返回新创建账户的公匙</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <p><strong>请求示例</strong></p>
+                <pre v-pre="" data-lang=""><code class="lang-">http://127.0.0.1:7890/account/generate</code></pre>
+                <p><strong>响应示例</strong></p>
+                <blockquote>
+                    <p>JSON格式</p>
+                </blockquote>
+                <pre v-pre="" data-lang=""><code class="lang-">{
+            "privateKey": "0962c6505d02123c40e858ff8ef21e2b7b5466be12c4770e3bf557aae828390f",
+            "address": "NCKMNCU3STBWBR7E3XD2LR7WSIXF5IVJIDBHBZQT",
+            "publicKey": "c2e19751291d01140e62ece9ee3923120766c6302e1099b04014fe1009bc89d3"
+        }</code></pre>
+
             </article>
         </div>
 
@@ -16,7 +54,7 @@
 
 <script>
     export default {
-        data: function(){
+        data: function () {
             return {}
         }
     }
