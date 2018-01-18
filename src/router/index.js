724e67a5cc5defdf043ece105450d022dcbabc63
blockshine <mark.ma@blockshine.com>
2018-01-18 15:31:32 +0800

    add transaction

--- src/router/index.js
+++ src/router/index.js
@@ -47,7 +47,7 @@ export default new Router({
                 },
                 {
                     path: '/transaction',
-                    component: resolve => require(['../components/page/transaction.vue'], resolve)
+                    component: resolve => require(['../components/page/transaction/transaction_add.vue'], resolve)
                 },
                 {
                    path:'/account_add',
